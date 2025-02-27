1913a9876f649a25427a76eea957dc8ff264b3ab
jessiebrownleo <sobengg5@gmail.com>
2025-02-27 15:29:29 +0700

    modify cors

--- obj/Debug/net9.0/DotnetAuthentication.AssemblyInfo.cs
+++ obj/Debug/net9.0/DotnetAuthentication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetAuthentication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7b7498d9e2c81a247a31abc5bc0472bd134f5b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetAuthentication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetAuthentication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
